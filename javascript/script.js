347a1fb049d5a9bcbd7f0c50e8055bc00581e80e
alekvan <aleksandar.vangelov@gmail.com>
2021-11-08 22:21:28 +0100

    js vezbi

---
+++ javascript/script.js
@@ -0,0 +1,14 @@
+let zbir = 0;
+
+for (let a = 0; a <= 10; a++) {
+  zbir = zbir + a;
+
+  if (a === 10) {
+    console.log(zbir);
+  }
+}
+let sum = 1;
+for (let k = 10; k >= 1; k--) {
+  sum = sum * k;
+}
+console.log(sum);

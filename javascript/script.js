8265918213a1bd91a2da85b8855b76d704d729d4
alekvan <aleksandar.vangelov@gmail.com>
2021-11-11 17:14:36 +0100

    some examples

--- javascript/script.js
+++ javascript/script.js
@@ -22,3 +22,16 @@ for (let i = 0; i < animals.length; i++) {
   combined.push(animals[i], foods[i]);
 }
 console.log(combined);
+
+let numberList = [4, 10, 42, 23, 15, 41, 35, 63, 76];
+let newList = [];
+let userIn = window.prompt(
+  `${numberList} 
+  Kolku elementi od nizata se potrebni`,
+  "Vnesi broj"
+);
+
+for (let i = userIn; i < numberList.length; i++) {
+  newList.push(numberList[i]);
+}
+console.log(newList);

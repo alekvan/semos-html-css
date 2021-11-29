1fc7456e92990256cf8ea34c15f0b9a6f11c29b2
alekvan <aleksandar.vangelov@gmail.com>
2021-11-29 17:16:48 +0100

    fixed some values, doesn't work

--- javascript/homework/classes.js
+++ javascript/homework/classes.js
@@ -14,14 +14,16 @@ class Kafemat {
       this.water -= neededWater;
       console.log(this.coffee, this.milk, this.water);
     } else {
-      if (this.coffee < 1) console.log(`Missing ${this.coffee + 7} coffee`);
-      else if (this.milk < 1) console.log(`Missing ${this.milk + 20} milk`);
-      else if (this.water < 1) console.log(`Missing ${this.water + 100} water`);
+      if (this.coffee < 7) console.log(`Missing ${7 - this.coffee} coffee`);
+      else if (this.milk < 20) console.log(`Missing ${20 - this.milk} milk`);
+      else if (this.water < 100)
+        console.log(`Missing ${100 - this.water} water`);
     }
   }
 }
 let coffee1 = new Kafemat(100, 100, 1000);
 
+coffee1.makeCoffee(7, 20, 100);
 coffee1.makeCoffee(7, 20, 100);
 coffee1.makeCoffee(7, 20, 100);
 coffee1.makeCoffee(7, 20, 100);
